ad763034105b50a97cdc65522f8df9c327fa0f56
Gustav Andersson <46231271+FanatiQS@users.noreply.github.com>
2026-03-04 01:54:34 +0100

    Fix gray badge for above 97% code coverage in lcov-badge2 (#242)
    * Fix short hex code not being detected
    
    * Only accept hex codes of 3 and 6 characters
    
    * Add tests for 3-digit hex codes and basic colors
    
    * Fix failing tests
    
    * Fix lint styling

--- src/color.spec.ts
+++ src/color.spec.ts
@@ -1,4 +1,4 @@
-import { getColorCode } from './color';
+import { getColorCode, basicColors } from './color';
 
 describe('color', () => {
     describe('getColorCode()', () => {
@@ -10,6 +10,22 @@ describe('color', () => {
             test('it accepts a 6-digit value with a leading #', () => {
                 expect(getColorCode('#123456')).toBe('#123456');
             });
+
+            test('it accepts a 3-digit value', () => {
+                expect(getColorCode('123')).toBe('#123');
+            });
+
+            test('it accepts a 3-digit value with a leading #', () => {
+                expect(getColorCode('#123')).toBe('#123');
+            });
+        });
+
+        describe('When the input is one of the basic colors', () => {
+            for (const [name, color] of Object.entries(basicColors)) {
+                test(`it accepts basic color "${name}"`, () => {
+                    expect(getColorCode(color)).toBe(color.toLowerCase());
+                });
+            }
         });
 
         describe('When the input is a color name', () => {
